329387a8594fc90084a20d8bc09a18c324c5c27e
Jacob H. Rothschild <20166610+JaHeRoth@users.noreply.github.com>
2026-08-28 12:33:14 -0700

    Finished ACTDecoder.forward

--- scripts/act.py
+++ scripts/act.py
@@ -24,7 +24,7 @@ class ACTEncoder(Module):
         batch_size = angle.size(0)
         angle_encoder_embedding: Tensor = self.angle_embedder(angle)  # (batch_size, encoder_dim)
         chunk_embedding: Tensor = self.chunk_embedder(chunk)  # (batch_size, chunk_length, encoder_dim)
-        chunk_encoder_input = torch.cat(  # (batch_size, chunk_length, encoder_dim)
+        chunk_encoder_input = torch.cat(  # (batch_size, chunk_length + 2, encoder_dim)
             [
                 self.cls_embedding.expand(batch_size, -1).unsqueeze(1),
                 angle_encoder_embedding.unsqueeze(1),
@@ -38,21 +38,22 @@ class ACTEncoder(Module):
 
 
 class ACTDecoder(Module):
-    def __init__(self, n_joints: int, d_model: int, z_dim: int):
-        self.decoder = Transformer(
-            d_model=d_model, nhead=8, num_encoder_layers=4, num_decoder_layers=7, dim_feedforward=3200, dropout=0.1
-        ) # transformer encoder-decoder
+    def __init__(self, n_joints: int, d_model: int, z_dim: int, latent_img_depth: int):
         self.latent_img_pos_embedding = TODO # sin-cos or learnable tensor
-        self.latent_img_embedder = TODO # linear layer
-        
+        self.latent_img_embedder = Linear(latent_img_depth, d_model) # linear layer
         self.angle_decoder_embedder = Linear(n_joints, d_model)
         self.z_embedder = Linear(z_dim, d_model)
+        self.decoder = Transformer(
+            d_model=d_model, nhead=8, num_encoder_layers=4, num_decoder_layers=7, dim_feedforward=3200, dropout=0.1
+        )
+        self.action_projector = Linear(d_model, n_joints)
 
     def forward(
         self,
         latent_img: Tensor,  # (batch_size, n_cameras, latent_depth, latent_height, latent_width)
         angle: Tensor,  # (batch_size, n_joints)
-        z: Tensor  # (batch_size, z_dim)
+        z: Tensor,  # (batch_size, z_dim)
+        chunk_length: int,
     ) -> Tensor:
         img_tokens = latent_img.permute(0, 1, 3, 4, 2)  # (batch_size, n_cameras, latent_height, latent_width, latent_depth)
         img_embeddings = (
@@ -62,7 +63,7 @@ class ACTDecoder(Module):
 
         angle_decoder_embedding = self.angle_decoder_embedder(angle)  # (batch_size, decoder_dim)
         z_embedding = self.z_embedder(z)  # (batch_size, decoder_dim)
-        decoder_input = torch.cat(
+        decoder_encoder_input = torch.cat(  # (batch_size, n_cameras * latent_height * latent_width + 2, decoder_dim)
             [
                 img_embeddings,
                 angle_decoder_embedding.unsqueeze(1),
@@ -71,7 +72,12 @@ class ACTDecoder(Module):
             dim=1,
         )
 
-        return self.decoder(decoder_input, TODO)
+        batch_size, d_model = z_embedding.size()
+        decoder_decoder_input = torch.zeros(batch_size, chunk_length, d_model)
+
+        decoder_output = self.decoder(decoder_encoder_input, decoder_decoder_input)
+        chunk = self.action_projector(decoder_output)
+        return chunk
 
 
 class ACT(Module):
