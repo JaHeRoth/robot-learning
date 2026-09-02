5a6e4e012decbe6887062316d9e9bfd33404ce8e
Jacob H. Rothschild <20166610+JaHeRoth@users.noreply.github.com>
2026-09-02 15:50:40 -0700

    Wrapped in generate_expert_trajectory

--- explorations/scene.py
+++ explorations/scene.py
@@ -4,6 +4,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from lerobot.datasets.lerobot_dataset import LeRobotDataset
 from time import time
+from tqdm import tqdm
 
 # %%
 path = "scenes/so100_reach/scene.xml"
@@ -56,7 +57,80 @@ fig.tight_layout()
 plt.show()
 
 # %%
-mujoco.mj_resetData(model, data)
+def sample_q_init_and_target():
+    q_init = (
+        model.key("home").qpos
+        + rng.normal(
+            loc=0.0,
+            scale=np.clip(
+                np.minimum(
+                    model.key("home").qpos - lb,
+                    ub - model.key("home").qpos,
+                ) / 2,
+                max=0.05,
+            ),
+            size=len(joint_names),
+        )
+    ).clip(lb, ub)
+
+    for _ in tqdm(range(100)):
+        q_target = rng.uniform(
+            center - 0.35 * span, center + 0.35 * span
+        )
+        data.qpos = q_target.copy()
+        mujoco.mj_forward(model, data)
+        data.mocap_pos[0] = data.site("tip").xpos.copy()
+
+        data.qpos = q_init.copy()
+        mujoco.mj_forward(model, data)
+
+        underground = data.mocap_pos[0][2] < 0.05
+        cramped = np.hypot(data.mocap_pos[0][0], data.mocap_pos[0][1]) < 0.12
+
+        renderer.update_scene(data, camera="front")
+        img = renderer.render()
+        cube_mask = (
+            (img[:, :, 0] > 150)
+            & (img[:, :, 1] < 100)
+            & (img[:, :, 2] < 100)
+        )
+        cube_visible = (
+            cube_mask[5:-5, 5:-5].sum() == cube_mask.sum()
+            and cube_mask.sum() >= 30
+        )
+        if not underground and not cramped and cube_visible:
+            return q_init, q_target
+    raise RuntimeError("goal sampling failed 100x")
+
+def capture_and_step():
+    renderer.update_scene(data, camera="front")
+    dataset.add_frame(
+        {
+            "observation.image": renderer.render(),
+            "observation.state": data.qpos.copy().astype(np.float32),
+            "action": data.ctrl.copy().astype(np.float32),
+        },
+        task=task,
+    )
+    for _ in range(20):
+        mujoco.mj_step(model, data)
+
+def generate_expert_trajectory():
+    mujoco.mj_resetData(model, data)
+    ctrl_steps = rng.integers(50, 100)
+    q_init, q_target = sample_q_init_and_target()
+
+    data.qpos = q_init.copy()
+    mujoco.mj_forward(model, data)
+
+    for step in range(1, ctrl_steps + 1):
+        data.ctrl = q_init + (step / ctrl_steps) * (q_target - q_init)
+        capture_and_step()
+    for _ in range(10):
+        capture_and_step()
+    dataset.save_episode()
+
+
 joint_names = [model.joint(i).name for i in range(model.njnt)]
 task = "Reach the red cube"
 dataset = LeRobotDataset.create(
@@ -85,79 +159,8 @@ rng = np.random.default_rng(0)
 lb, ub = model.jnt_range[:, 0], model.jnt_range[:, 1]
 center, span = (lb + ub) / 2, ub - lb
 
-q_init = (
-    model.key("home").qpos
-    + rng.normal(
-        loc=0.0,
-        scale=np.clip(
-            np.minimum(
-                model.key("home").qpos - lb,
-                ub - model.key("home").qpos,
-            ) / 2,
-            max=0.05,
-        ),
-        size=len(joint_names),
-    )
-).clip(lb, ub)
-
-for _ in range(100):
-    q_target = rng.uniform(
-        center - 0.35 * span, center + 0.35 * span
-    )
-    data.qpos = q_target.copy()
-    mujoco.mj_forward(model, data)
-    data.mocap_pos[0] = data.site("tip").xpos.copy()
-
-    
-    data.qpos = q_init.copy()
-    mujoco.mj_forward(model, data)
-
-    underground = data.mocap_pos[0][2] < 0.05
-    cramped = np.hypot(data.mocap_pos[0][0], data.mocap_pos[0][1]) < 0.12
-
-    renderer.update_scene(data, camera="front")
-    img = renderer.render()
-    cube_mask = (
-        (img[:, :, 0] > 150)
-        & (img[:, :, 1] < 100)
-        & (img[:, :, 2] < 100)
-    )
-    cube_visible = (
-        cube_mask[5:-5, 5:-5].sum() == cube_mask.sum()
-        and cube_mask.sum() >= 30
-    )
-    if not underground and not cramped and cube_visible:
-        break
-if underground or cramped or not cube_visible:
-    raise Exception
-
-ctrl_steps = rng.integers(50, 100)
-for step in range(1, ctrl_steps + 1):
-    data.ctrl = q_init + (step / ctrl_steps) * (q_target - q_init)
-    renderer.update_scene(data, camera="front")
-    dataset.add_frame(
-        {
-            "observation.image": renderer.render(),
-            "observation.state": data.qpos.copy().astype(np.float32),
-            "action": data.ctrl.copy().astype(np.float32),
-        },
-        task=task,
-    )
-    for _ in range(20):
-        mujoco.mj_step(model, data)
-for _ in range(10):
-    renderer.update_scene(data, camera="front")
-    dataset.add_frame(
-        {
-            "observation.image": renderer.render(),
-            "observation.state": data.qpos.copy().astype(np.float32),
-            "action": data.ctrl.copy().astype(np.float32),
-        },
-        task=task,
-    )
-    for _ in range(20):
-        mujoco.mj_step(model, data)
-dataset.save_episode()
-
+num_episodes = 100
+for _ in range(num_episodes):
+    generate_expert_trajectory()
 
 # %%
