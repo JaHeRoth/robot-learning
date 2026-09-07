a125937ee29aa56f43ce09d00dea7127264ca025
Jacob H. Rothschild <20166610+JaHeRoth@users.noreply.github.com>
2026-09-07 12:52:07 -0700

    Sketching eval loop in training loop

--- scripts/train_act.py
+++ scripts/train_act.py
@@ -1,11 +1,15 @@
 from functools import partial
 
+import imageio
 import torch
 from lerobot.datasets.lerobot_dataset import LeRobotDataset
 from torch.optim import AdamW
 from torch.utils.data import DataLoader
+from lerobot.envs.factory import make_env, make_env_config
+from gymnasium.vector import VectorEnv
 
-from scripts.act import ACT
+from scripts.my_rollout import my_rollout
+from scripts.act import ACT, ACTPolicy
 from scripts.train_common import train_loop
 
 
@@ -22,6 +26,18 @@ def act_loss(model, batch, stats, kl_weight):
     return l1_loss + kl_weight * kl_loss
 
 
+def eval_act(env: VectorEnv, policy: ACTPolicy, eval_seeds: list[int], step: int, n_recorded: int):
+    out = my_rollout(env, policy, eval_seeds)
+    success_rate = out["success"].any(dim=1).float().mean()
+    print(f"step {step}: success={success_rate:.3f}")
+
+    for i in range(n_recorded):
+        done_i = out["done"][i].argmax()
+        imageio.mimsave(uri=TODO, imgs=out["pixels"][:done_i], fps=10)
+
+    return dict(success_rate=success_rate)
+
+
 def train_act(seed: int = 0):
     torch.manual_seed(seed)
 
@@ -33,6 +49,10 @@ def train_act(seed: int = 0):
     grad_clip_at = 10.0
 
     num_batches = 100_000
+    n_action_steps = 16
+    eval_every = 1000
+    n_eval_envs = 50
+    n_recorded = 10
 
     fps = 10
     ds = LeRobotDataset(
@@ -54,6 +74,12 @@ def train_act(seed: int = 0):
         for obj in ["action", "observation.state"]
     }
 
+    env = make_env(make_env_config("pusht"), n_envs=n_eval_envs)
+    eval_seeds = list(range(1000, 1000 + n_eval_envs))
+    def eval_fn(model, step):
+        policy = ACTPolicy(model, dataset_stats=stats, n_action_steps=n_action_steps)
+        return eval_act(env, policy, eval_seeds, step, n_recorded)
+
     train_loop(
         model=model,
         loader=loader,
@@ -63,6 +89,8 @@ def train_act(seed: int = 0):
         out_dir="outputs/my_act",
         stats=stats,
         grad_clip_at=grad_clip_at,
+        eval_every=eval_every,
+        eval_fn=eval_fn,
     )
 
 
